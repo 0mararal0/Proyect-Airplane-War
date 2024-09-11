df6bc4bf969dd5e54d40e61ba4722fc219cab842
alberto marcos <amarcosarranz@gmail.com>
2024-09-11 18:18:31 +0200

    miercoles2 maquetacion y hiScore

--- js/main.js
+++ js/main.js
@@ -4,9 +4,10 @@ const startScreenNode = document.querySelector("#start-screen");
 const gameScreenNode = document.querySelector("#game-screen");
 const gameOverScreenNode = document.querySelector("#game-over-screen");
 const gameBoxNode = document.querySelector("#game-box");
+const hiScorePoint = document.querySelector("#hiScorePoint");
 //botones
 const startBtnNode = document.querySelector("#start-btn");
-const restartGame = document.querySelector("#restart-game");
+const restartGame = document.querySelector("#restart-game-btn");
 
 /* ··········Variables globales del juego·········· */
 //declaramos player-declaramos disparo-parametros comunes
@@ -21,6 +22,11 @@ let positionPlayerX = 50;
 let positionPlayerY = 300;
 let enemyPlaneArr = [];
 let hiScore = "0000";
+let local = JSON.parse(localStorage.getItem("HiScore"));
+console.log(local);
+
+let hiScoreArr = local;
+let timeRemaining = 20;
 const typewriter = document.getElementById("typewriter");
 
 /* ··········Funciones globales del juego·········· */
@@ -38,7 +44,7 @@ const textTypewriter = (text = "", time = 200) => {
   }, time);
 };
 textTypewriter(
-  "Desde tiempos inmemoriables, los habitantes de Aetheria estan en guerra, tu mision es acabar con los enemigos y restaurar de nuevo la paz. Suerte!",
+  "Desde tiempos inmemorables, los habitantes de Aetheria estan en guerra, tu mision es acabar con los enemigos y restaurar de nuevo la paz. Suerte!",
   120
 );
 
@@ -51,6 +57,32 @@ function startGame() {
 
   // añadir elementos al juego
   player = new PlayerPlane(positionPlayerX, positionPlayerY, playerH, playerW);
+
+  //añadir temporizador
+  //pasamos a minutes
+  const minutes = Math.floor(timeRemaining / 60)
+    .toString()
+    .padStart(2, "0");
+  //pasamos a segundos
+  const seconds = (timeRemaining % 60).toString().padStart(2, "0");
+  //mostramos en el DOM
+  const timeRemainingContainer = document.getElementById("timeRemaining");
+  timeRemainingContainer.innerText = `${minutes}:${seconds}`;
+  //generamos el intervalo
+  let timerId = setInterval(() => {
+    timeRemaining--;
+    const minutes = Math.floor(timeRemaining / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (timeRemaining % 60).toString().padStart(2, "0");
+    timeRemainingContainer.innerText = `${minutes}:${seconds}`;
+
+    //console.log(quiz.timeRemaining)
+    if (timeRemaining === 0) {
+      clearInterval(timerId);
+      gameOver();
+    }
+  }, 1000);
 
   //iniciar Intervalo de juego
   gameIntervalId = setInterval(() => {
@@ -72,6 +104,7 @@ function gameLoop() {
   outFire();
   outEnemyPlane();
   detectFire();
+  detectCollisionPlayer();
 }
 
 //añadimos disparos
@@ -90,12 +123,14 @@ function outFire() {
     fireArr.splice(0, 1);
   }
 }
+
 //añadimos aviones enemigos
 function addEnemyPlane() {
   let randomPositionY = Math.floor(Math.random() * 600);
   let newPlane = new EnemyPlane(randomPositionY);
   enemyPlaneArr.push(newPlane);
 }
+
 //eliminamos aviones enemigos en DOM y en js
 function outEnemyPlane() {
   if (enemyPlaneArr.length === 0) {
@@ -109,40 +144,72 @@ function outEnemyPlane() {
 
 //detectamos disparos en avones enemigos y los borramos del DOM
 function detectFire() {
-  enemyPlaneArr.forEach((elem) => {
-    if (
-      fireArr[0].x < elem.x + elem.w &&
-      fireArr[0].x + fireArr[0].w > elem.x &&
-      fireArr[0].y < elem.y + elem.h &&
-      fireArr[0].y + fireArr[0].h > elem.y
-    ) {
-      elem.node.remove();
-      fireArr[0].node.remove();
-      fireArr.splice(0, 1);
-      count();
-    }
+  enemyPlaneArr.forEach((enemyPlane, indexEnemyPlane) => {
+    fireArr.forEach((fire, indexFire) => {
+      if (
+        fire.x < enemyPlane.x + enemyPlane.w &&
+        fire.x + fire.w > enemyPlane.x &&
+        fire.y < enemyPlane.y + enemyPlane.h &&
+        fire.y + fire.h > enemyPlane.y
+      ) {
+        enemyPlaneArr[indexEnemyPlane].node.remove();
+        enemyPlaneArr.splice(indexEnemyPlane, 1);
+        fireArr[indexFire].node.remove();
+        fireArr.splice(indexFire, 1);
+        count();
+      }
+    });
   });
-
   //añadimos puntuacion al contador por cada avion enemigo eliminado
+
+  const hiScoreValue = document.getElementById("hiScoreValue");
+  hiScoreValue.textContent = hiScore.toString().padStart(4, "0");
   function count() {
-    hiScore = hiScore * 50;
+    hiScore = parseInt(hiScore) + 50;
     const hiScoreValue = document.getElementById("hiScoreValue");
     hiScoreValue.textContent = hiScore.toString().padStart(4, "0");
-    console.log(hiScore);
   }
+}
+
+function detectCollisionPlayer() {
+  enemyPlaneArr.forEach((enemyPlane, indexEnemyPlane) => {
+    if (
+      player.x < enemyPlane.x + enemyPlane.w &&
+      player.x + player.w > enemyPlane.x &&
+      player.y < enemyPlane.y + enemyPlane.h &&
+      player.y + player.h > enemyPlane.y
+    ) {
+      enemyPlaneArr[indexEnemyPlane].node.remove();
+      enemyPlaneArr.splice(indexEnemyPlane, 1);
+      hiScoreArr.push(hiScore);
+      localStorage.setItem("HiScore", JSON.stringify(hiScoreArr));
+      gameOver();
+    }
+  });
 }
 
 //fin del juego reiniciamos pantallas
 function gameOver() {
+  console.log(hiScoreArr);
+
   clearInterval(gameIntervalId);
   clearInterval(enemyPlaneIntervalId);
   gameOverScreenNode.style.display = "flex";
-  startScreenNode.style.display = "none";
+  gameScreenNode.style.display = "none";
   gameBoxNode.style.display = "none";
+  hiScoreArr.forEach((elem) => {
+    let hiScoreNode = document.createElement("p");
+    hiScoreNode.innerHTML = elem;
+    hiScorePoint.append(hiScoreNode);
+    console.log(elem);
+  });
 }
 
 /* ··········Event Listeners·········· */
 startBtnNode.addEventListener("click", startGame);
+restartGame.addEventListener("click", () => {
+  location.reload();
+});
 window.addEventListener("keydown", (event) => {
   if (event.key === "ArrowUp") {
     player.up();
